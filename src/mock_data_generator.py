3aa81ead24c51181fe8cfd41d76154090d043c7d
Jeff Den Bleyker <denb42@gmail.com>
2016-11-15 15:07:35 -0500

    Adjust the speed to 1-5kph; add cmdline args for days and start/stop time; impact data is optional; print upload elapsed time.

--- src/mock_data_generator.py
+++ src/mock_data_generator.py
@@ -5,13 +5,16 @@ Usage: mock_data_generator.py [options]
 Options:
     -e <entity_count>, --entity-count=<entity_count>  Number of entities to generate [default: 100]
     -p <update_period>, --period=<update_period>  Time (secs) between location updates [default: 15]
-    -m <max_speed>, --max-speed=<max_speed>  Max forklift speed (KPH)) [default: 30]
+    -m <max_speed>, --max-speed=<max_speed>  Max forklift speed (KPH)) [default: 5]
     -c <confidence>, --conf-dist=<distance>  Max location confidence radius (feet) [default: 1000.0]
-    -d <start_date>, --start-date=<date>  Start date [default: 2016-09-01]
+    -d <start_date>, --start-date=<date>  Start date [default: 2016-01-01]
+    -y <days>, --days=<n>  Number of days from start date to generate
+    -s <start_time>, --start-time=<time>  Start time (HH:MM) [default: 06:00]
+    -p <stop_time>, --stop-time=<time>  Stop time (HH:MM) [default: 18:00]
     -h <host>, --host=<server_name>  Name of Conduce server [default: dev-app.conduce.com]
     -a <api_key>, --apikey=<key>  API Key
     -x <cmx_dataset>, --cmx-dataset=<id>  CMX Dataset ID (Entities)
-    -i <ims_dataset>, --ims-dataset=<id>  IMS Dataset ID (Impacts)
+    -i <ims_dataset>, --ims-dataset=<id>  Optional IMS Dataset ID (Impacts)
     -g <format> --gen=<format>  Generate output in the given format (JSON | CSV) only; no uploading
 
 """
@@ -36,7 +39,7 @@ ENTITY_COUNT=100
 entities=[]
 
 # Pick whether repeatable data is generated or not
-RANDOM_SEED=13
+RANDOM_SEED=101
 #RANDOM_SEED=time.utcnow()
 
 # Generate data for output in the given format; no uploading
@@ -51,13 +54,13 @@ COORDS_LL=0
 COORDS_XY=1
 
 # >>> Comment/uncomment one of these blocks
-#MAP_COORDS=COORDS_XY
-#MAP_BOTTOM_LEFT=(0.0,0.0)
-#MAP_TOP_RIGHT=(150.0,160.0)
+MAP_COORDS=COORDS_XY
+MAP_BOTTOM_LEFT=(0.0,0.0)
+MAP_TOP_RIGHT=(150.0,160.0)
 
-MAP_COORDS=COORDS_LL
-MAP_BOTTOM_LEFT=(30.00,-100.02)
-MAP_TOP_RIGHT=(30.02,-100.00)
+#MAP_COORDS=COORDS_LL
+#MAP_BOTTOM_LEFT=(30.00,-100.02)
+#MAP_TOP_RIGHT=(30.02,-100.00)
 
 
 #
@@ -120,8 +123,8 @@ def getDegreesPerMeter( bl, tr ):
 # -------------------- Speed/Distance
 
 # Range of entity speeds (in KPH)
-SPEED_MIN_KPH=3
-SPEED_MAX_KPH=10
+SPEED_MIN_KPH=1
+SPEED_MAX_KPH=5
 
 X_AXIS_METERS=0
 Y_AXIS_METERS=0
@@ -168,25 +171,44 @@ UPDATE_PERIOD_S=15
 
 TIME_OFFSET = int(round((datetime.now() - datetime.utcnow()).total_seconds()))
 
-START_DATE='2016-09-01'
+START_DATE='2016-01-01'
 START_TIME='06:00'
 STOP_TIME='18:00'
 
-#
 # Concatenate the date/time and return the time in seconds.
-#
 def getTime(dateStr, timeStr):
     date_time = dateStr + " " + timeStr
     return int( time.mktime( time.strptime(date_time, '%Y-%m-%d %H:%M') ) )
 
-
-#
 # Parse the date for the month and return how many days.
-#
 def getDays(dateStr):
     dt_obj = datetime.strptime(dateStr, '%Y-%m-%d')
     n = monthrange(dt_obj.year, dt_obj.month)[1]
     return n
+
+# Print the time elapsed
+def printElapsedTime(strMsg, startTime, endTime):
+    hours, rem = divmod(endTime - startTime, 3600)
+    minutes, seconds = divmod(rem, 60)
+    if minutes > 0:
+        print "%s %02d mins %02.3f secs" % (strMsg, int(minutes), seconds)
+    else:
+        print "%s %02.3f secs" % (strMsg, seconds)
+
+# Delay the required amount til the next update period
+def delayTime(periodSecs, startTime, endTime):
+    hours, rem = divmod(endTime - startTime, 3600)
+    minutes, seconds = divmod(rem, 60)
+    if minutes > 0:
+        totalElapsed = (minutes * 60) + seconds
+    else:
+        totalElapsed = seconds
+
+    delaySecs = 0.0
+    if totalElapsed < periodSecs:
+        delaySecs = float(periodSecs) - float(totalElapsed)
+        print "Delay %02.3f" % (delaySecs)
+        time.sleep(delaySecs)
 
 
 # -------------------- Entity operations
@@ -198,9 +220,9 @@ def getDays(dateStr):
 MIN_CONFIDENCE_VALUE=10.0
 MAX_CONFIDENCE_VALUE=1000.0
 
-def getPosConfidenceMeters():
-    # Generate a confidence in feet and convert to meters
-    return int(round(0.3048 * random.triangular(MIN_CONFIDENCE_VALUE, MAX_CONFIDENCE_VALUE, 320.0), 0))
+# Convert confidence factor in (ft) to substrate units (mm)
+def getPositionConfidence():
+    return int(round(0.3048 * 1000 * random.triangular(MIN_CONFIDENCE_VALUE, MAX_CONFIDENCE_VALUE, 320.0), 0))
 
 #
 # Init and return a list of entities.
@@ -267,12 +289,12 @@ def getNextMove( posX, posY ):
         y = max( (posY - dyPct), 0.0 )
 
     elif ( move == MOVE_LEFT ):
-        hdg = 270
+        hdg = 90
         dxPct = getDistancePercent( d, X_AXIS_METERS )
         x = max( (posX - dxPct), 0.0 )
 
     else: # MOVE_RIGHT
-        hdg = 90
+        hdg = 270
         dxPct = getDistancePercent( d, X_AXIS_METERS )
         x = min( (posX + dxPct), 100.0 )
 
@@ -289,7 +311,7 @@ def updateEntityPosition( entity ):
     hdg = entity.get("attrs")[0]
     hdg["int64-value"] = newMove[0]
     conf = entity.get("attrs")[1]
-    conf["int64-value"] = getPosConfidenceMeters()
+    conf["int64-value"] = getPositionConfidence()
 
 # Update all the entities locations for the given time
 def updateLocations(tm):
@@ -427,7 +449,7 @@ def waitForUploadJob(authStr, jobURL):
     
     finished = False
     while not finished:
-        time.sleep(0.5)
+        time.sleep(1.0)
         #print jobURL
         response = requests.get(jobURL, headers=headers)
         if int(response.status_code / 100) != 2:
@@ -449,7 +471,7 @@ def uploadEntities(apiKey, datasetId, hostServer, timestampMs, ents):
         printEntities(ents)
         return
 
-    print datetime.fromtimestamp(timestampMs).strftime('%Y-%m-%d %H:%M:%S')
+    strGenDate = datetime.fromtimestamp(timestampMs).strftime('%Y-%m-%d %H:%M:%S')
 
     authStr = 'Bearer ' + apiKey
     URI = '/conduce/api/datasets/add_datav2/' + datasetId
@@ -459,22 +481,24 @@ def uploadEntities(apiKey, datasetId, hostServer, timestampMs, ents):
         'Content-type': 'application/json',
         'Content-Length': len(payload)
     }
+    print "%s: Uploading..." % (strGenDate)
     #print headers
-
-    print "Uploading ", hostServer, URI
     #print payload
 
+    tStart = time.time()
     connection = httplib.HTTPSConnection(hostServer)
     connection.request("POST", URI, payload, headers)
     response = connection.getresponse()
     print response.status, response.reason, response.read()
     connection.close()
 
-    #wait for the job to finish
+    # Wait for the job to finish
     job_loc = response.getheader('location')
     if job_loc:
         jobURL = "https://%s/conduce/api%s" % (hostServer, job_loc)
         waitForUploadJob( authStr, jobURL )
+        tStop = time.time()
+        printElapsedTime( strGenDate + " Duration:", tStart, tStop )
     else:
         print "Error: Response contains no job location."
 
@@ -488,6 +512,9 @@ def main():
     global UPDATE_PERIOD_S
     global SPEED_MAX_KPH
     global START_DATE
+    global START_TIME
+    global STOP_TIME
+
     global MAP_COORDS
     global MAX_CONFIDENCE_VALUE
     global GENONLY_FORMAT
@@ -507,6 +534,19 @@ def main():
     SPEED_MAX_KPH = int(arguments.get('--max-speed'))
     MAX_CONFIDENCE_VALUE = float(arguments.get('--conf-dist'))
     START_DATE = arguments.get('--start-date')
+    START_TIME = arguments.get('--start-time')
+    STOP_TIME = arguments.get('--stop-time')
+
+    startDateTime = getTime(START_DATE, START_TIME) + TIME_OFFSET
+    stopDateTime = getTime(START_DATE, STOP_TIME) + TIME_OFFSET
+
+    nDays = 1
+    strDays = arguments.get('--days')
+    if strDays == None:
+        nDays = getDays(START_DATE)
+    else:
+        nDays = int(strDays)
+    #print "Date %s %s-%s Days=%d" %(START_DATE, START_TIME, STOP_TIME, nDays)
 
     hostServer=None
     apiKey=None
@@ -526,15 +566,13 @@ def main():
             print "CMX Dataset ID required."
             return
         if datasetIdIMS == 'None':
-            print "IMS Dataset ID required."
-            return
+            datasetIdIMS = None
+
         #print apiKey
         #print datasetIdCMX
         #print datasetIdIMS
 
-    nDays = getDays(START_DATE)
-    startDateTime = getTime(START_DATE, START_TIME) + TIME_OFFSET
-    stopDateTime = getTime(START_DATE, STOP_TIME) + TIME_OFFSET
+    # -----------------
 
     initAxisDimensions(MAP_BOTTOM_LEFT, MAP_TOP_RIGHT)
     initEntities(ENTITY_COUNT, startDateTime, (0,0));
@@ -549,7 +587,7 @@ def main():
         uploadEntities(apiKey, datasetIdCMX, hostServer, startDateTime, mapped)
 
         while ( tm < stopDateTime ):
-            if tm >= impactTm:
+            if datasetIdIMS and tm >= impactTm:
                 impact = getImpactEntity( mapped[random.randint(0, ENTITY_COUNT-1)], getImpact() )
                 impacts.append(impact)
                 #print "Impact:", impact
@@ -558,11 +596,13 @@ def main():
             tm += UPDATE_PERIOD_S
             updateLocations(tm)
             mapped = getMappedEntityPositions()
+            tBeg = time.time()
             uploadEntities(apiKey, datasetIdCMX, hostServer, tm, mapped)
+            tEnd = time.time()
+            delayTime(UPDATE_PERIOD_S, tBeg, tEnd)
 
-        # Upload the day's impacts
-        #print "Numberof Impacts Today:", len(impacts)
-        if len(impacts) > 0:
+        if datasetIdIMS and len(impacts) > 0:
+            #print "Numberof Impacts Today:", len(impacts)
             #print impacts
             uploadEntities(apiKey, datasetIdIMS, hostServer, tm, impacts)
             # Empty the list
